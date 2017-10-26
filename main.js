65ee76d5c12626ceeabeea0e461f33923275b133
Joseph Papaluca <jpapaluc@gmail.com>
2017-10-26 16:14:22 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// this app needs to:
+
+// 1. get an input from the webpage
+//     -from the <input> tag on the page
+//     -in the form of a string?
+
+// 2. sort through the given string to find individual moves(inputs/characters)
+//      -recognize directional and attack inputs to start
+//      -add in commas & brackets later
+//      -eventually include special modifiers, stances, etc.
+
+// 3a. replace the given characters with the appropriate input icon
+//      -ie. "d/f" becomes <i class="movelist d-f"></i>
+//
+//    OR:
+//
+// 3b. return an error if the given input is invalid.
+
+// 4. render the new combo on the screen. 
