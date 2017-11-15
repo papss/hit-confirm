6ee78c1ce56b5ffa169898071812740f9b000833
Joseph Papaluca <jpapaluc@gmail.com>
2017-11-15 16:35:49 -0500

    context menu selections work

--- chrome/src/javascript/eventPage.js
+++ chrome/src/javascript/eventPage.js
@@ -1,7 +1,26 @@
-var contextMenuItem = {
-  "id": "hitConfirm",
-  "title": "Hit Confirm",
-  "contexts": ["selection"]
-}
+// var contextMenuItem = {
+//   "id": "hitConfirm",
+//   "title": "Hit Confirm",
+//   "contexts": ["selection"],
+// };
+//
+// chrome.contextMenus.create(contextMenuItem);
+//
+// chrome.contextMenus.onCLicked.addListener(function(clickedItem) {
+//   if (clickedItem.menuItemId == "hitConfirm" && clickedItem.selectionText) {
+//     console.log(clickedItem.selectionText);
+//   };
+// });
 
-chrome.contextMenus.create(contextMenuItem);
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "title": "HitConfirm",
+    "id": "hitConfirmMenu",
+    "contexts": ["selection"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(clickedItem) {
+  var test = clickedItem.selectionText;
+  window.alert(test); 
+})
