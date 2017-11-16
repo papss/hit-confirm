7909a74608045aa72a05ed7df35cd2613f9503d0
Joseph Papaluca <jpapaluc@gmail.com>
2017-11-16 17:44:06 -0500

    added panel toggle for popup.html

--- chrome/src/javascript/popup.js
+++ chrome/src/javascript/popup.js
@@ -3,4 +3,27 @@
 
 $(document).ready(function() {
 
+  // toggle the active panel:
+  var activeSection = document.getElementsByClassName('active-section');
+
+  $('.hc-header').on('click', function() {
+    $(activeSection).removeClass('active-section');
+    var className = $(this).attr('class');
+    switch (className) {
+
+      case 'hc-header title':
+        console.log("you clicked the title");
+        $('.hc-main').addClass('active-section');
+        break;
+
+      case 'hc-header header-legend':
+        $('.hc-legend').addClass('active-section');
+        break;
+
+      case 'hc-header header-options':
+        $('.hc-options').addClass('active-section');
+        break;
+    }
+  });
+
 });
