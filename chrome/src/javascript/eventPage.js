c4f8c1f44d5f356493f94942148bae5ded022197
Joseph Papaluca <jpapaluc@gmail.com>
2017-12-15 16:39:06 -0500

    selected text now passes into new popup

--- chrome/src/javascript/eventPage.js
+++ chrome/src/javascript/eventPage.js
@@ -1,3 +1,4 @@
+// this is a background script.
 // Creates the context menu item:
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
@@ -11,7 +12,8 @@ chrome.runtime.onInstalled.addListener(function() {
 function contextClicked(info, tab) {
   if (info.menuItemId == "hitConfirmMenu") {
     chrome.tabs.query({
-      active: true, currentWindow: true
+      active: true,
+      currentWindow: true
     }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         "action": "openPopUp"
@@ -22,8 +24,7 @@ function contextClicked(info, tab) {
 
 chrome.contextMenus.onClicked.addListener(contextClicked);
 
-
-// old Click event listener: 
+// old Click event listener:
 
 // chrome.contextMenus.onClicked.addListener(function(clickedItem) {
 //   var test = clickedItem.selectionText;
--- chrome/src/javascript/placeholder.js
+++ chrome/src/javascript/placeholder.js
@@ -1,15 +1,19 @@
+// this is a content script.
 // placeholder code for creating extension lightbox:
-
+// listens for the openPopUp message to be sent
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if ( request.action == "openPopUp" ) {
 
-    // injects pop-up HTML into the page: 
+    // gets the selected text: 
+    input = window.getSelection().toString();
+
+    // injects pop-up HTML into the page:
     background = document.createElement('div');
     background.id = "hc-background";
     hcBox = document.createElement('main');
     hcBox.id = "hc-test";
 
-    hcBox.innerHTML = '<h1> hello friends </h1>';
+    hcBox.innerHTML = input;
 
     document.body.appendChild(background);
     background.appendChild(hcBox);
