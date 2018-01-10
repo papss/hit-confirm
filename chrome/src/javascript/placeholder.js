2be20b396cf393b298bfb71ce1d2bfd7850eb7db
Joseph Papaluca <jpapaluc@gmail.com>
2018-01-10 18:07:08 -0500

    implemente hitConfirm method for input on button press, need to fix link to icon image

--- chrome/src/javascript/placeholder.js
+++ chrome/src/javascript/placeholder.js
@@ -34,12 +34,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     document.body.appendChild(background);
     background.appendChild(hcBox);
 
-    // runs hitConfirm method on button press:
-
     // adds listener to button:
     var hcButton = document.getElementById('hc-btn');
     hcButton.onclick = function(event) {
-      document.getElementById('hc-output').innerHTML = input; 
+      hitConfirm(input);
     };
 
     // Closes the Lightbox (duh):
@@ -59,7 +57,210 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 
-
-
-
 // hitConfirm method (re-written to remove jQuery):
+function hitConfirm(input) {
+  // split the string into an array of substrings:
+  var inputArray = input.split(' ');
+  console.log(inputArray);
+  // loop over each substring (item):
+  var output = inputArray.forEach(function(item) {
+
+  // Future revision: figure out how to streamline this.
+    var separateNumbers =     item.replace(/([udbfn]|[UDBFN])(?=[1-4])/g, "$1 ")
+    var separatePlus =        separateNumbers.replace(/([udbfn]|[UDBFN])\+/g, '$1 + ' );
+    var separateTilde =       separatePlus.replace(/\~/g, ' ~ ');
+    var separateOr =          separateTilde.replace(/\_/g, ' or ');
+    var sepOpenParenthesis =  separateOr.replace(/\(/g, ' ( ');
+    var sepCloseParenthesis = sepOpenParenthesis.replace(/\)/g, ' ) ');
+    var separateComma =       sepCloseParenthesis.replace(/\,/g, ' , ');
+
+    console.log(separateComma);
+
+    var finalInput = separateComma.split(' ');
+
+    console.log(finalInput);
+
+    finalInput.forEach(function(item) {
+
+      var outputContainer = document.getElementById('hc-output');
+
+      switch (item) {
+
+        // if neutral:
+        case "N":
+          console.log("neutral");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist neutral'></i>");
+          break;
+
+        // if forward:
+        case "f":
+          console.log("forward");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist forward'></i>");
+          break;
+
+        case "F":
+          console.log("hold forward");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-forward'></i>");
+          break;
+
+        // if back:
+        case "b":
+          console.log("back");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist back'></i>");
+          break;
+
+        case "B":
+          console.log("hold back");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-back'></i>");
+          break;
+
+        // if up:
+        case "u":
+          console.log("up");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist up'></i>");
+          break;
+
+        case "U":
+          console.log("hold up");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-up'></i>");
+          break;
+
+        // if down:
+        case "d":
+          console.log("down");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist down'></i>");
+          break;
+
+        case "D":
+          console.log("hold down");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-up'></i>");
+          break;
+
+        // if up/forward:
+        case "u/f":
+        case "uf":
+          console.log("up/forward");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist u-f'></i>");
+          break;
+
+        case "U/F":
+        case "UF":
+          console.log("hold up-forward");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-uf'></i>");
+          break;
+
+        // if up/back:
+        case "u/b":
+        case "ub":
+          console.log("up/back");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist u-b'></i>");
+          break;
+
+        case "U/B":
+        case "UB":
+          console.log("hold up-back");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-ub'></i>");
+          break;
+
+        // if down/forward:
+        case "d/f":
+        case "df":
+          console.log("down/forward");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist d-f'></i>");
+          break;
+
+        case "D/F":
+        case "DF":
+          console.log("hold down-forward");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-df'></i>");
+          break;
+
+        // if down/back:
+        case "d/b":
+        case "db":
+          console.log("down/back");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist d-b'></i>");
+          break;
+
+        case "D/B":
+        case "DB":
+          console.log("hold down-back");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist hold-db'></i>");
+          break;
+
+        // if 1 (left punch):
+        case "1":
+          console.log("1 (left punch)");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist one'></i>");
+          break;
+
+        // if 2 (right punch):
+        case "2":
+          console.log("2 (right punch)");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist two'></i>");
+          break;
+
+        // if 3 (left kick):
+        case "3":
+          console.log("3 (left kick)");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist three'></i>");
+          break;
+
+        // if 4 (right kick):
+        case "4":
+          console.log("4 (right kick)");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist four'></i>");
+          break;
+
+        // if 1+2:
+        case "1+2":
+          console.log("1 + 2");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist one-two'></i>");
+          break;
+
+        // if 1+3:
+        case "1+3":
+          console.log("1 + 3");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist one-three'></i>");
+          break;
+
+        // if 1+4:
+        case "1+4":
+          console.log("1 + 4");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist one-four'></i>");
+          break;
+
+        // if 2+3:
+        case "2+3":
+          console.log("2 + 3");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist two-three'></i>");
+          break;
+
+        // if 2+4:
+        case "2+4":
+          console.log("2 + 4");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist two-four'></i>");
+          break;
+
+        // if 3+4:
+        case "3+4":
+          console.log("3 + 4");
+          outputContainer.insertAdjacentHTML("beforeend", "<i class='movelist three-four'></i>");
+          break;
+
+        // no log message on space:
+        case " ":
+          break;
+
+        // else (unrecognized/unstyled input):
+        default:
+          console.log("unrecognized " + item);
+          outputContainer.insertAdjacentHTML("beforeend", item + " ");
+
+      };
+
+    });
+
+  });
+
+}
