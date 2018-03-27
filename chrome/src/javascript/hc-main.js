4af3de9242b9aba82eaaa043fbe4ae3cdbe553fd
Joseph Papaluca <jpapaluc@gmail.com>
2018-03-27 18:07:13 -0400

    fun with the chrome storage api

--- chrome/src/javascript/hc-main.js
+++ chrome/src/javascript/hc-main.js
@@ -2,11 +2,24 @@
 
 $(document).ready(function() {
 
+  // value set to html input field:
   var inputToSave = document.getElementById('input-to-save');
 
-  document.getElementById('save-form').addEventListener('submit', function(e) {
-    e.preventDefault;
-    console.log(inputToSave.value);
-  })
+  $('#save-btn').on('click', function() {
+    var theValue = inputToSave.value;
+    if (!theValue) {
+      message('no value');
+      return;
+    }
+    chrome.storage.local.set({"value": theValue}, function() {
+      console.log("the value is set to: " + theValue);
+    })
+  });
 
-})
+  $('#test-btn').on('click', function() {
+    chrome.storage.local.get(["value"], function(result) {
+      console.log("the value currently is: " + result.key);
+    });
+  });
+
+});
